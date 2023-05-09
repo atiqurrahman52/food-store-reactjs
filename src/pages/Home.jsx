9a817b5d2e29addd9bf6e87cfb4f5121fe273bec
atiqurrahman52 <atiqur.rahman@intelli.global>
2023-05-09 18:56:10 +0600

    exotic food updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import Banner from "../components/home/Banner";
 import DeliveryProcess from "../components/home/DeliveryProcess";
+import ExoticFood from "../components/home/ExoticFood";
 import FeaturedProduct from "../components/home/FeaturedProduct";
 import HeroSection from "../components/home/HeroSection";
 import Test from "../components/home/Test";
@@ -17,6 +18,7 @@ const Home = () => {
             <Banner />
             <DeliveryProcess />
             <Testimonial />
+             <ExoticFood />
         </div>
     );
 };
