acbf32d24cee95297c341af0599e85ce3414752f
atiqurrahman52 <atiqur.rahman@intelli.global>
2023-05-14 15:15:30 +0600

    updated

--- src/components/cart/CartBanner.jsx
+++ src/components/cart/CartBanner.jsx
@@ -1,20 +1,19 @@
-
 const CartBanner = () => {
-    return (
-        <section>
-    <div className="sub-header h-[300px] bg-cover bg-center bg-no-repeat hidden md:block">
-      <h3 className="flex justify-center h-full items-center font-Playfair font-black text-xl md:text-5xl text-text_color">My Cart</h3>
-      
-    </div>
+  return (
+    <div>
+      <div className="sub-header h-[300px] bg-cover bg-center bg-no-repeat hidden md:block">
+        <h3 className="flex justify-center h-full items-center font-Playfair font-black text-xl md:text-5xl text-text_color">
+          My Cart
+        </h3>
+      </div>
 
-    {/* <!-- for mobile  --> */}
-    <div className="sub-header-mobile h-[180px] bg-cover bg-center bg-no-repeat md:hidden">
-      <h3 className="flex justify-center h-full items-center font-Playfair font-black text-xl md:text-5xl text-text_color">My Cart</h3>
-      
+      <div className="sub-header-mobile h-[180px] bg-cover bg-center bg-no-repeat md:hidden">
+        <h3 className="flex justify-center h-full items-center font-Playfair font-black text-xl md:text-5xl text-text_color">
+          My Cart
+        </h3>
+      </div>
     </div>
-    
-  </section>
-    );
+  );
 };
 
 export default CartBanner;
