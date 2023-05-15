7fb38854cd2cfe5836dcd6f908e6d4398099034c
atiqurrahman52 <atiqur.rahman@intelli.global>
2023-05-15 13:05:21 +0600

    updated

--- src/components/shop/ShopProduct.jsx
+++ src/components/shop/ShopProduct.jsx
@@ -1,12 +1,13 @@
 import { ArrowLeft, ArrowRight } from "@phosphor-icons/react";
 import productData from "../../data/product/productData";
-import { Link } from "react-router-dom";
 // import { useState } from "react";
+import ProductCard from "./ProductCard";
 
 const ShopProduct = () => {
   //   const [counter, setCounter] = useState();
   //   const [increment, setIncrement] = useState();
-  //   const [decrement, setDecrement] = useState()
+  //   const [decrement, setDecrement] = useState();
+  //   const [quantity, setQuantity] = useState();
 
   return (
     <div className="container">
@@ -106,88 +107,9 @@ const ShopProduct = () => {
         </div>
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-          {productData.map(
-            ({ id, img, productName, productDescription, price }) => (
-              <div
-                key={id}
-                className="border border-[#EBEBEE] overflow-hidden rounded-lg md:rounded-2xl shadow-[0_0px_42px_rgba(119,119,119,0.05)]"
-              >
-                <Link to="/product-details">
-                  <img className="mb-4 xl:mb-[17px] w-full" src={img} alt="" />
-                </Link>
-
-                <div className="p-6 pt-4">
-                  <h3 className="text-center border-b border-[#CCCCCC] pb-3 xl:pb-4 font-Montserrat font-medium text-sm md:text-[22px] text-text_color">
-                    {productName}
-                  </h3>
-                  <p className="text-center font-Montserrat text-xs md:text-sm text-p_text mt-4 mb-3 xl:mb-6">
-                    {productDescription}
-                  </p>
-                  <p className="font-Montserrat font-semibold text-xl md:text-[32px] text-text_color text-center mb-4">
-                    ${price}
-                  </p>
-
-                  <div className="mx-auto rounded-xl w-[256px] xl:w-[308px] h-10 xl:h-12 overflow-hidden">
-                    <button className="cart-add-btn bg-[#EBEBEE] w-full h-full font-Montserrat font-medium text-sm md:text-base text-text_color mb-6">
-                      Add to cart
-                    </button>
-
-                    <div
-                      className="btn-group counter-btn flex justify-between items-center px-8 w-full h-full bg-primary_3 text-white"
-                      role="group"
-                      aria-label="Basic outlined example"
-                    >
-                      <button
-                        type="button"
-                        className="add-cart remove-btn text-2xl"
-                      >
-                        <svg
-                          width="25"
-                          height="24"
-                          viewBox="0 0 25 24"
-                          fill="none"
-                          xmlns="http://www.w3.org/2000/svg"
-                        >
-                          <path
-                            d="M20.2358 12H5.23584"
-                            stroke="white"
-                            strokeWidth="1.5"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                          />
-                        </svg>
-                      </button>
-                      <input
-                        disabled=""
-                        className="text-center w-20 bg-transparent text-2xl"
-                        value="1"
-                      />
-                      <button
-                        type="button"
-                        className="add-cart add-btn text-2xl"
-                      >
-                        <svg
-                          width="25"
-                          height="24"
-                          viewBox="0 0 25 24"
-                          fill="none"
-                          xmlns="http://www.w3.org/2000/svg"
-                        >
-                          <path
-                            d="M12.2646 4.5V19.5M19.7646 12H4.76465"
-                            stroke="white"
-                            strokeWidth="1.5"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                          />
-                        </svg>
-                      </button>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            )
-          )}
+          {productData.map((item, i) => (
+            <ProductCard key={i} data={item} />
+          ))}
         </div>
 
         <div className="flex justify-between items-center mt-5">
